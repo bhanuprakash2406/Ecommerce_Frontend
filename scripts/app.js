19ff36f08bec9900f6c72c2a1e0636ceb82d98cd
bhanuprakash2406 <bhanuprakash2606@gmail.com>
2026-02-12 01:55:06 +0530

    Add files via upload

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,5 @@
-console.log("E-Commerce Website Loaded");
+const nav = document.querySelector('nav');
+const menuIcon = document.querySelector('.bi-list');
+menuIcon.addEventListener('click', () => {
+    nav.classList.toggle('active');
+});
